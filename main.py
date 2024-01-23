64b4fbbc860053320723b1ab9456d5701bc5d5d0
NestorZaum <nestorzaum.rabelo@gmail.com>
2024-01-22 21:02:42 -0300

    inicio

--- main.py
+++ main.py
@@ -1,65 +1,70 @@
-import emoji
-from random import randint
+from random import randint, choice, shuffle
 from time import sleep
 
+print('-=' * 15)
+print('\33[32mVamos Jogar Jokenpo!!!\33[m')
+print('-=' * 15)
+sleep(2)
 
 itens = ('Pedra', 'Papel', 'Tesoura')
-adversario = ('Ana', 'Paula', 'José', 'Heitor', 'Rafael', 'Felipe')
+adversario = ['Ana', 'Paula', 'José', 'Heitor', 'Rafael', 'Felipe']
 computador = randint(0, 2)
-player2 = randint(0, 4)
+shuffle(adversario)
+player2 = choice(adversario)
 
-player1 = input('Qual seu nome? ')
-print(f'Bem vindo {player1}')
-print('Sorteando adversário...')
+player1 = input('Qual seu nome? ').title().strip()
+print(f'\nBem vindo {player1}')
+print('\nSorteando adversário...')
 sleep(2)
-print(f'Seu adversário é {adversario[player2]}')
+print(f'\nSeu adversário é {player2}')
 sleep(2)
-print('\nVamos Jogar!!!')
-sleep(2)
-print(emoji.emojize('\nSuas opções: \n[0] Pedra :raised_fist:\n[1] Papel :raised_hand:\n[2] Tesoura :victory_hand:'))
+
+print('\nSuas opções: \n\033[34m[0] Pedra \n[1] Papel \n[2] Tesoura \033[m')
 jogador = int(input('Qual é sua jogada? '))
+
 print('JO')
 sleep(1)
 print('KEN')
 sleep(1)
 print('PO!!!')
 sleep(1)
+
 print('-=' * 15)
-print(f'O {adversario[player2]} jogou {itens[computador]}')
+print(f'O {player2} jogou {itens[computador]}')
 print(f'Você jogou {itens[jogador]}')
 print('-=' * 15)
 
 cont1 = 0
 cont2 = 0
 
-if computador == 0:  # computador jogou Pedra
+if computador == 0:  # player2 jogou Pedra
     if jogador == 0:
         print('EMPATE')
     elif jogador == 1:
-        print(emoji.emojize(f'{player1} VENCE :partying_face:'))
+        print(f'{player1} VENCE')
         cont1 += 1
     elif jogador == 2:
-        print(emoji.emojize(f'{adversario[player2]} VENCE :partying_face:'))
+        print(f'{player2} VENCE')
         cont2 += 1
     else:
         print('JOGADA INVÁLIDA!')
-elif computador == 1:  # computador jogou Papel
+elif computador == 1:  # player2 jogou Papel
     if jogador == 0:
-        print(emoji.emojize(f'{adversario[player2]} VENCE :partying_face:'))
+        print(f'{player2} VENCE')
         cont2 += 1
     elif jogador == 1:
         print('EMPATE')
     elif jogador == 2:
-        print(emoji.emojize(f'{player1} VENCE :partying_face:'))
+        print(f'{player1} VENCE')
         cont1 += 1
     else:
         print('JOGADA INVÁLIDA!')
-elif computador == 2:  # computador jogou Tesoura
+elif computador == 2:  # player2 jogou Tesoura
     if jogador == 0:
-        print(emoji.emojize(f'{player1} VENCE :partying_face:'))
+        print(f'{player1} VENCE')
         cont1 += 1
     elif jogador == 1:
-        print(emoji.emojize(f'{adversario[player2]} VENCE :partying_face:'))
+        print(f'{player2} VENCE')
         cont2 += 1
     elif jogador == 2:
         print('EMPATE')
@@ -68,4 +73,4 @@ elif computador == 2:  # computador jogou Tesoura
 
 print('\n')
 print(f'{player1} tem {cont1} vitorias')
-print(f'{adversario[player2]} tem {cont2} vitorias')
+print(f'{player2} tem {cont2} vitorias')
